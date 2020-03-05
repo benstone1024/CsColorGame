1e8ebe0f260ba01a7e0c07832a3aa2cb67cfbfb7
Benjamin J Stone <38769350+benstone1024@users.noreply.github.com>
2020-03-05 12:01:23 -0500

    Add files via upload
    uploading the half way point of the colt steele color game

---
+++ colorGame.js
@@ -0,0 +1,65 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+colorDisplay.textContent = pickedColor;
+
+
+
+for(var i = 0; i <squares.length; i++) {
+    // add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+    
+     squares[i].addEventListener("click", function(){
+
+    var clickedColor = this.style.backgroundColor;
+
+    if(clickedColor === pickedColor){
+        messageDisplay.textContent = "Correct!";
+        changeColors(clickedColor);
+        h1.style.background = clickedColor;
+    } else {
+        this.style.backgroundColor = "#232323"
+        messageDisplay.textContent = "Try Again"
+    }
+
+    });
+
+}
+function changeColors(color){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+function generateRandomColors(num){
+    var arr = []
+
+    for(var i = 0; i < num; i++){
+      arr.push(randomColor())  
+    }
+    return arr;
+}
+function randomColor(){
+    var r = math.floor(math.random() * 256);
+
+    var g = math.floor(math.random() * 256);
+
+    var b = math.floor(math.random() * 256);
+    return "rgb("+ r +"," + g + "," + b + ")";
+}
